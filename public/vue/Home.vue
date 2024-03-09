18b9303f25daf39f8f6609ddbe5b5a1fb8af9261
kurita2099 <takuri00+github@gmail.com>
2024-03-09 13:43:49 +0900

    ルームのリスト取得

--- public/vue/Home.vue
+++ public/vue/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <div>Home</div>
+
+<ul>
+<!-- items 配列の要素を item として繰り返しレンダリングする -->
+<li v-for="item in roomIds" :key="item.id">
+<!-- item がオブジェクトであると仮定し、そのプロパティを表示する -->
+{{ item.timestamp }}: {{ item.host }}
+</li>
+</ul>
   </div>
 </template>
 
@@ -8,7 +16,23 @@
 module.exports = {
   data() {
     return {
+        ref: {},
+		//items: [{name:"2222",price:10000}],
+        roomIds: [],
     }
+  },
+  created: async function() {
+   //DB参照
+   setTimeout(()=>
+			db.ref("/ox/") 
+                .on("value", (data)=> {
+                    if (data) {
+                        const rootList = data.val();
+                        
+                        this.roomIds = rootList;
+                        console.log(this.roomIds)
+                    }
+                }),1000);
   },
 }
 </script>
