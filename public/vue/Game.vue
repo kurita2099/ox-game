37a88da727beb1953c1af2cb8370368367f35966
kurita2099 <takuri00+github@gmail.com>
2024-03-09 13:04:51 +0900

    削除しておく　

--- public/vue/Game.vue
+++ public/vue/Game.vue
@@ -45,21 +45,7 @@ beforeDestroy() {
 this.game.destroy(true)
 },
 methods: {
-// Phaser3のシーンのメソッドを定義する
-preload() {
-    console.log(this)
-// 画像や音声などのアセットをロードする
-},
-create() {
-// ゲームのオブジェクトやロジックを作成する
-  console.log(this)
-//this.load.image('logo', '../assets/logo.png')
-//this.add.image(400, 300, 'logo')
-},
-update() {
-// ゲームの状態を更新する
-  console.log(this);
-}
+
 }
 }
 </script>
