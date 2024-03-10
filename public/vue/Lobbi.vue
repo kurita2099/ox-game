c3693ca14bc26570a3ed8aff94d96d57e7a94134
kurita2099 <takuri00+github@gmail.com>
2024-03-10 15:25:12 +0900

    ルームの参加者を配列に追加

--- public/vue/Lobbi.vue
+++ public/vue/Lobbi.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
     <div>ABOUT</div>
-  
   </div>
 </template>
 
 <script>
 module.exports = {
-  
-
   data() {
     return {
-      roomIds:[]
-    }
+      roomIds: [],
+    };
   },
-  props: ['message'],
-  created: async function() {
-   //DB参照
-   
-   console.log(this.global.userId)
-   /*
+  props: ["message"],
+  created: async function () {
+    //DB参照
 
-			db.ref("/ox/") 
+    console.log(this.global.userId);
+    var name = prompt("名前を入力してください");
+    // ルームのデータベース参照を取得する
+    //var database = firebase.database(); // データベースのインスタンスを取得する
+    var roomId = "room1"; // ルームのIDを指定する
+    var roomRef = db.ref("ox/" + this.global.roomId); // ルームのデータベース参照を取得する
+
+    // ルームの参加者であるmembersのデータベース参照を取得する
+    var membersRef = roomRef.child("members"); // membersのデータベース参照を取得する
+
+    // ルームの参加者であるmembersに新しいユーザーのIDと名前を追加する
+    var userId = this.global.userId; // 新しいユーザーのIDを指定する
+    var userName = name; // 新しいユーザーの名前を指定する
+    membersRef.child(userId).set(userName); // membersに新しいユーザーのIDと名前を追加する
+    /* 
+
+			db.ref("/ox/"+this.global.roomIdId) 
                 .on("value", (data)=> {
                     if (data) {
                         //const rootList = data.val();
@@ -30,7 +40,7 @@ module.exports = {
                         console.log(this.roomIds)
                     }
                 })
-    */
+  */
   },
-}
+};
 </script>
